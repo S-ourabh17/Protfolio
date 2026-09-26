c0aa0814d97472d8ea55e549eba3a5b1540932e9
S-ourabh17 <srbh9920@gmail.com>
2026-09-26 23:05:24 +0530

    Update Resume href

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -207,7 +207,7 @@ function App() {
                         <a href= "https://www.linkedin.com/in/sourabh-7223a3362/" target="_blank">
                             LinkedIn
                         </a>
-                        <a href="Sourabh_Resume.docx" target="_blank" rel="noopener noreferrer">
+                        <a href="/Sourabh_Resume.docx" target="_blank" rel="noopener noreferrer">
                             View Resume
                         </a>
                         <a href = "https://leetcode.com/u/Sharma_Srbh/" target="_blank">
